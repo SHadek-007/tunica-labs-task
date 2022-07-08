7f30aabe6dbef932bd3bc108a394d4e8d1f45d4a
SADEK HOSSAIN <shadeks16@gmail.com>
2022-07-08 12:23:27 +0600

    signin design done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './Components/Header';
 import { Routes, Route } from "react-router-dom";
 import SignUp from './Pages/Login/SignUp';
+import SignIn from './Pages/Login/SignIn';
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
-        <Route path='/' element={<SignUp></SignUp>}></Route>
+        <Route path='/' element={<SignIn></SignIn>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
